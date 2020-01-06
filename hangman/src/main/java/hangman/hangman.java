84a4224ae1e40996915b131e30fedcb98dec9796
tkeating <tkeating@click2mail.com>
2020-01-06 17:08:43 -0500

    chore:add gradle project, add acm library
    Add gradle so as to avoid putting dependency in classpath

---
+++ hangman/src/main/java/hangman/hangman.java
@@ -0,0 +1,12 @@
+package hangman;
+
+import acm.util.*;
+
+public class hangman {
+
+    public static void main(String args[]) {
+        System.out.println('x');
+        RandomGenerator rg = new RandomGenerator();
+    }
+
+}
